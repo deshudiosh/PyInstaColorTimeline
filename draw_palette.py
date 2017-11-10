fd465b119b608f8b170f567a135d0e5b41569fbd
deshu <deshudiosh@gmail.com>
2017-11-10 01:18:09 +0100

    multiprocessed rows making, quick whole pallete routine

--- draw_palette.py
+++ draw_palette.py
@@ -1,4 +1,6 @@
 import os
+from functools import partial
+from multiprocessing.pool import Pool
 from typing import Sequence
 
 import more_itertools
@@ -6,56 +8,73 @@ from PIL import Image, ImageDraw
 from colorthief import ColorThief
 
 
-def get_rows(username: str, width: int = 600, img_per_row: int = 3, image_samples: int = 3):
+#TODO: use 'center fill' html equivalent
+def _resize_fill(img: Image) -> Image:
+    pass
+
+def _make_row(username: str, width: int, img_per_row: int, image_samples: int, path: str, size: int, row_items: list):
+
+    row_num = row_items.pop()
+
+    out_img = Image.new('RGB', (width, size), color='white')
+    draw = ImageDraw.Draw(out_img)
+
+    i = 0
+
+    for idx, path in enumerate(row_items):
+        resized = Image.open(path).resize((size - 1, size))
+        out_img.paste(resized, box=(size * idx, 0))
+
+        rect_h = size / image_samples / img_per_row
+
+        for color in ColorThief(path).get_palette(color_count=image_samples):
+            sx = size * img_per_row
+            ex = width
+            sy = rect_h * i
+            ey = sy + rect_h
+            draw.rectangle([sx, sy, ex, ey], fill=color)
+            i += 1
+
+    out_img.save('./users/{}/rows/{}.jpg'.format(username, row_num))
+    out_img.close()
+
+
+
+def make_rows(username: str, width: int = 600, img_per_row: int = 3, image_samples: int = 3):
     # TODO: read filenames from json
     # TODO: sort by date
-    path = "./users/{}/".format(username)
-    img_paths = [path + s for s in os.listdir(path) if str(s).endswith('.jpg')]  # type: Sequence[str]
 
     size = int(width / (img_per_row + 1))
-    rows = more_itertools.chunked(img_paths, img_per_row)
-    palettes = []
 
-    for row in rows:
-        out_img = Image.new('RGB', (width, size), color='white')
-        draw = ImageDraw.Draw(out_img)
+    path = "./users/{}/".format(username)
+    img_paths = [path + s for s in os.listdir(path) if str(s).endswith('.jpg')]
+    rows = list(more_itertools.chunked(img_paths, img_per_row))
 
-        i = 0
+    # put row_index into row_list as last item for later popping in mapped function
+    for idx, row in enumerate(rows):
+        row.append(idx)
 
-        for idx, path in enumerate(row):
-            resized = Image.open(path).resize((size-1, size))
-            out_img.paste(resized, box=(size*idx, 0))
-
-            rect_h = size/image_samples/img_per_row
-
-            for color in ColorThief(path).get_palette(image_samples):
-                sx = size*img_per_row
-                ex = width
-                sy = rect_h*i
-                ey = sy + rect_h
-                draw.rectangle([sx, sy, ex, ey], fill=color)
-                i += 1
-
-        palettes.append(out_img)
-        # yield out_img
-
-    return palettes
-
-def get_whole(username:str, width: int = 600, img_per_row: int = 3, image_samples: int = 3):
-    rows = get_rows(username, width, img_per_row, image_samples)
-    row_height = int(width / (img_per_row + 1))
-    total_height = row_height * len(rows)
-
-    palette = Image.new('RGB', (width, total_height))
-
-    for i, row in enumerate(rows):
-        palette.paste(row, box=(0, row_height*i))
-
-    palette.save('./users/{}.jpg'.format(username))
-    # palette.show()
+    pool = Pool()
+    func = partial(_make_row, username, width, img_per_row, image_samples, path, size)
+    pool.map(func, rows)
 
 
+def whole_from_rows(username: str):
+    path = './users/{}/rows/'.format(username)
+    img_paths = sorted([path + s for s in os.listdir(path) if str(s).endswith('.jpg')])
 
+    images = list(map(Image.open, img_paths))
+    widths, heights = zip(*(i.size for i in images))
 
+    total_height = sum(heights)
+    max_width = max(widths)
 
+    out_img = Image.new('RGB', (max_width, total_height))
+
+    y_offset = 0
+    for img in images:
+        out_img.paste(img, box=(0, y_offset))
+        y_offset += img.size[1]
+
+    out_img.save('./users/{}.jpg'.format(username))
 
--- main.py
+++ main.py
@@ -2,26 +2,24 @@ import os
 
 import draw_palette
 import get_images
+import timing
 
 
-user = ['design.travel.cats',
-        'deshudiosh',
-        'lofot.love',
-        'tworczywo',
-         'matowe_spojrzenie'][0]
+def main():
+    user = ['design.travel.cats',
+            'deshudiosh',
+            'lofot.love',
+            'tworczywo',
+            'matowe_spojrzenie'][0]
+
+    timing.init()
+
+    # get_images.by_username(user)
+
+    # draw_palette.make_rows(user, 1200, 9, 3)
+
+    draw_palette.whole_from_rows(user)
 
 
-# get_images.by_username(user)
-# get_images.by_username_external(user)
-
-
-# draw_palette.get_rows('design.travel.cats')
-draw_palette.get_whole(user, 600, 2, 4)
-
-
-# def draw_color_for_img(file_name, color):
-#     image = Image.new("RGB", (100, 100), color).save(file_name)
-
-# for file_id in itertools.chain.from_iterable(weeks.values()):
-#     ct = ColorThief('{}/{}.jpg'.format(user_folder, file_id))
-#     draw_color_for_img('{}/_{}.jpg'.format(user_folder, file_id), ct.get_color())
+if __name__ == '__main__':
+    main()
